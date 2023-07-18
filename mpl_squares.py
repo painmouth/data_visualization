253ded605623c11504fcbe5fa316d33574743f9c
MrQ1V <thepainmouth@gmail.com>
2023-07-18 21:06:54 +0300

    -

---
+++ mpl_squares.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+input_value = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+fix, ax = plt.subplots()
+ax.plot(input_value, squares, linewidth=2)
+
+# Назначение заголовка диаграммы и меток осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Назначение размера шрифта делений на осях.
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
